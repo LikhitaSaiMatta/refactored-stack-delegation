f3d6cf87173965740cebb5a0988a2bac376e27b5
likhitasaimatta <likhitasaimatta0701@gmail.com>
2024-03-06 20:00:20 -0500

    Refactor : Introduced delegate in MyStack and initialized as this in the constructor of MyStack

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -3,7 +3,13 @@ package org.example;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
-public class MyStack<E> extends ArrayList<E> {
+public class MyStack<E> extends ArrayList<E>{
+
+    private ArrayList<E> delegate;
+
+    public MyStack(){
+        this.delegate = this;
+    }
 
     public void push(E e) {
         add(e);
